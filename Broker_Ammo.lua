d75520010f9934a214ddd9e3377a667f228dfd7f
Raphael Mader <mader.raphael@gmx.at>
2019-09-25 17:19:11 +0200

    Change to an event-based approach

--- Broker_Ammo.lua
+++ Broker_Ammo.lua
@@ -1,28 +1,40 @@
+local frame = CreateFrame("Frame")
 local dataObject = LibStub("LibDataBroker-1.1"):NewDataObject("Broker_Ammo", {type = "data source", text = ""})
 
 local CTimerAfter = C_Timer.After
 local GetInventoryItemCount = GetInventoryItemCount
 local GetInventoryItemTexture = GetInventoryItemTexture
 
-local ammoSlotId = GetInventorySlotInfo("AMMOSLOT")
+local ammoSlotId
 
 local function CheckAmmo()
-	local texture = GetInventoryItemTexture("player", ammoSlotId)
+	if (ammoSlotId ~= nil) then
+		local texture = GetInventoryItemTexture("player", ammoSlotId)
 
-	if (texture ~= nil) then
-		local amount = tostring(GetInventoryItemCount("player", ammoSlotId))
+		if (texture ~= nil) then
+			local amount = tostring(GetInventoryItemCount("player", ammoSlotId))
 
-		dataObject.text = amount
-		dataObject.value = amount
-		dataObject.icon = texture
-	else
-		dataObject.text = ""
-		dataObject.value = nil
-		dataObject.icon = nil
+			dataObject.text = amount
+			dataObject.value = amount
+			dataObject.icon = texture
+		else
+			dataObject.text = ""
+			dataObject.value = nil
+			dataObject.icon = nil
+		end
 	end
-
-	-- update each second
-	CTimerAfter(1, CheckAmmo)
 end
 
-CheckAmmo()
+local function Initialize()
+	ammoSlotId = GetInventorySlotInfo("AMMOSLOT")
+	CheckAmmo()
+end
+
+local eventHandlers = {
+	PLAYER_LOGIN = Initialize,
+	UNIT_INVENTORY_CHANGED = CheckAmmo,
+	BAG_UPDATE = CheckAmmo
+  }
+
+  for event in pairs(eventHandlers) do frame:RegisterEvent(event) end
+  frame:SetScript("OnEvent", function (_, event, ...) eventHandlers[event](...) end)
